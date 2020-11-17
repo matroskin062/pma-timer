e0ccb8867daf3f79d4830d3bb85e436b3b4c8bdf
Dima Matroskin <dima.matroskin062@gmail.com>
2020-11-17 22:14:25 +0200

    com

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(15 * 60);
+  const [play, setPlay] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    let interval = null;
+    if (play) {
+      interval = setTimeout(() => {
+        if (seconds) setSeconds((s) => s - 1);
+      }, 1000);
+    } else {
+      clearTimeout(interval);
+    }
+    return () => clearTimeout(interval);
+  });
+
+  const handleClick = () => {
+    if (inputRef.current.value) {
+      setSeconds(
+        inputRef.current.value
+          .split(':')
+          .map((el) => parseInt(el))
+          .reduce((acc, cur) => acc * 60 + cur)
+      );
+    }
+    setPlay(!play);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Time'>
+        {seconds === 0
+          ? 'STOP'
+          : `0${Math.floor(seconds / 60)}`.substr(-2) +
+            ':' +
+            `0${seconds % 60}`.substr(-2)}
+        {/* {`0${Math.floor(seconds / 60)}`.substr(-2)}:
+        {`0${seconds % 60}`.substr(-2)} */}
+      </div>
+      <div className='Controls'>
+        <input type='time' defaultValue='15:00' ref={inputRef} />
+        <button onClick={handleClick}>{!play ? 'Start' : 'Pause'}</button>
+      </div>
     </div>
   );
 }
